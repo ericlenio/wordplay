018f44943499750d4762218a0938ffe053babd38
Eric Lenio <eric@lincware.com>
2026-01-04 21:10:51 -0700

    make into PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = 'wordplay-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/wordplay.css',
+  '/wordplay.js',
+  '/gemini/icon.svg'
+];
+
+// Install event: cache the assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(assetsToCache);
+      })
+  );
+});
+
+// Fetch event: serve assets from cache, or fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
