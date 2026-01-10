30e66d3ba8dce126d83c3dbd41e3ff26e06901f5
Eric Lenio <eric@lincware.com>
2026-01-10 06:35:10 -0700

    move icon

--- wordplay.js
+++ wordplay.js
@@ -1,4 +1,4 @@
-const VERSION = "1.0.32";
+const VERSION = "1.0.33";
 // --- Configuration ---
 const DICT_URL_COMMON = "https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english.txt";
 const DICT_URL_SCRABBLE = "https://raw.githubusercontent.com/jesstess/Scrabble/master/scrabble/sowpods.txt";
